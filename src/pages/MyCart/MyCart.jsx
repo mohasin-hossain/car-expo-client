4ae987573a9427c994ec7f84219c8027f76ded36
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-15 13:19:30 +0600

    Initial Setup and Routing

---
+++ src/pages/MyCart/MyCart.jsx
@@ -0,0 +1,10 @@
+
+const MyCart = () => {
+    return (
+        <div>
+            <h1>MyCart</h1>
+        </div>
+    );
+};
+
+export default MyCart;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import Home from "../pages/Home/Home";
+import AddProduct from "../pages/AddProduct/AddProduct";
+import MyCart from "../pages/MyCart/MyCart";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/add-product",
+        element: <AddProduct></AddProduct>,
+      },
+      {
+        path: "/my-cart",
+        element: <MyCart></MyCart>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/Register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default routes;
